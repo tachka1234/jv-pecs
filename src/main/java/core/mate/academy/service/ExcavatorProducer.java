1cd5c96bc690b402ddbd1b457ec7b1154d2b7ccb
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-05-01 17:37:40 +0300

    fixed lints

--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -1,8 +1,6 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Excavator;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class ExcavatorProducer implements MachineProducer<Excavator> {
